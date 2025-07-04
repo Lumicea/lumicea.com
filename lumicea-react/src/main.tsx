2e947e31d79d99d7663b7379b26131907f70a920
Lumicea <swyatt@lumicea.com>
2025-07-04 01:50:15 +0100

    Updated main.tsx

--- lumicea-react/src/main.tsx
+++ lumicea-react/src/main.tsx
@@ -9,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
-      <CookieConsent />
     </AuthProvider>
   </React.StrictMode>,
 )
