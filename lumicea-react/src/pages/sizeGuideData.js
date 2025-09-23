f710e296d3e99cf7217cb6fd15e453e236cbabe0
Lumicea <swyatt@lumicea.com>
2025-09-23 15:17:17 +0100

    Create sizeGuideData.js

---
+++ lumicea-react/src/pages/sizeGuideData.js
@@ -0,0 +1,46 @@
+export const gaugeThicknessData = [
+  { gauge: '22G', thickness: '0.6mm' },
+  { gauge: '20G', thickness: '0.8mm' },
+  { gauge: '18G', thickness: '1.0mm' },
+  { gauge: '16G', thickness: '1.2mm' },
+  { gauge: '14G', thickness: '1.6mm' },
+];
+
+export const insideDiameterData = [
+  { 
+    size: '6mm', 
+    inches: '15/64"', 
+    comfort: 'Snug', 
+    bestFor: 'Petite features, first piercing, minimal look' 
+  },
+  { 
+    size: '7mm', 
+    inches: '9/32"', 
+    comfort: 'Standard', 
+    bestFor: 'Most common size, everyday wear' 
+  },
+  { 
+    size: '8mm', 
+    inches: '5/16"', 
+    comfort: 'Comfortable', 
+    bestFor: 'Standard comfort fit, versatile styling' 
+  },
+  { 
+    size: '9mm', 
+    inches: '23/64"', 
+    comfort: 'Roomy', 
+    bestFor: 'Larger features, statement piece' 
+  },
+  { 
+    size: '10mm', 
+    inches: '25/64"', 
+    comfort: 'Very Roomy', 
+    bestFor: 'Bold, dramatic look, maximum comfort' 
+  },
+  { 
+    size: '12mm', 
+    inches: '15/32"', 
+    comfort: 'Extra Roomy', 
+    bestFor: 'Large statement pieces, spacious fit' 
+  },
+];
